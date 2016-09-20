f618bbabcb14107c032606f4a14a1a6fa8f4c7dc
Katherine Broner <katherine.broner@gmail.com>
2016-09-20 15:58:59 -0400

    create additional routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to => 'application#index'
+
+  get 'about' => 'application#about'
+  get 'issues' => 'application#issues'
+  get 'giving-back' => 'application#giving-back'
+  get 'connect' => 'application#connect'
 end
