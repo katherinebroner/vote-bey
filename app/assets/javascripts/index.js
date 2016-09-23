ca30fcbf8dac1d4056420a9942f78164d5306ec5
Katherine Broner <katherine.broner@gmail.com>
2016-09-23 06:43:43 -0400

    add smooth scroll feature

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,13 @@
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
 $(document).ready(function(){
   $(".single-img").mouseover(function(){
     $(this).css('opacity', 0.2);
