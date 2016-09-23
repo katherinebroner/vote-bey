24c2f3d18284a8ac02d3c6bbfd5f954bdd2296b4
Katherine Broner <katherine.broner@gmail.com>
2016-09-22 20:02:12 -0400

    add mouseover feature for images

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,10 +1,9 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-// $(function() {
-//     $('a.page-scroll').bind('click', function(event) {
-//         var $anchor = $(this);
-//         $('html, body').stop().animate({
-//             scrollTop: $($anchor.attr('href')).offset().top
-//         }, 1500, 'easeInOutExpo');
-//         event.preventDefault();
-//     });
-// });
+$(document).ready(function(){
+  $(".single-img").mouseover(function(){
+    $(this).css('opacity', 0.2);
+  });
+  $(".single-img").mouseleave(function(){
+    $(this).css('opacity', '');
+  });
+});
+
